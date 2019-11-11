96510cf7d7701c509885b40512916eade0a49284
Janna Pierre <gojanna@aol.com>
2019-11-11 16:06:31 -0500

    Deleted unused code from JobRepository.

--- job-api/src/main/java/com/example/jobapi/repository/JobRepository.java
+++ job-api/src/main/java/com/example/jobapi/repository/JobRepository.java
@@ -4,9 +4,8 @@ import com.example.jobapi.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
-//    void delete(Optional<Job> );
+
 }
