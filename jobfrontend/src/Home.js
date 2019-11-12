694f1e7aa672599b1db39589520e32afa08f9a29
Janna Pierre <gojanna@aol.com>
2019-11-12 14:15:57 -0500

    Moved search btn in form.

--- jobfrontend/src/Home.js
+++ jobfrontend/src/Home.js
@@ -16,8 +16,9 @@ const Home = (props) => {
         id="searchJob"
         placeholder="Enter Job Title"
         />
-        </form>
         <input type="submit" value="Search" />
+        </form>
+
       </div>
     );
 }
