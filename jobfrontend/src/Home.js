c6b1a62fc03aeb2128c230580d0166aee590beb9
Janna Pierre <gojanna@aol.com>
2019-11-14 10:29:36 -0500

    [HOMEJS]Removed unused code, working to refactor.

--- jobfrontend/src/Home.js
+++ jobfrontend/src/Home.js
@@ -5,19 +5,7 @@ const Home = (props) => {
     return(
       <div className="Home">
 
-        <h1>Search for a Job</h1>
-
-        <form onSubmit={props.submitForm} className="searchJob">
-        <input
-        type="text"
-        label="searchJob"
-        value={props.searchJob}
-        onChange={props.handleSearchJobChange}
-        id="searchJob"
-        placeholder="Enter Job Title"
-        />
-        <input type="submit" value="Search" />
-        </form>
+        <h1>Welcome</h1>
 
       </div>
     );
